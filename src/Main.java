c8265883090599a41fd491acaf5484c6811de414
amberk887 <amber.kusma@quinnipiac.edu>
2021-04-12 20:26:47 -0400

    add button

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Main extends Frame {
     private static String _path;
     private Button _pullButton;
+    private Button _addButton;
     private Scanner _input;
     public Main(){
         _pullButton = new Button(java.awt.Color.RED, "Pull", _path, 500, 200);
-
+        _addButton = new Button(java.awt.Color.BLUE, "Add", _path, 500,100);
     }
     public static void main(String[] args) {
         Scanner _input = new Scanner(System.in);
