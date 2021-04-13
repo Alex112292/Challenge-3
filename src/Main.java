21e62e9cbc5f955e471489613659a3eb80936e12
Alex112292 <57815789+Alex112292@users.noreply.github.com>
2021-04-13 17:08:40 -0400

    Commit

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,8 @@ public class Main extends Frame {
     private Button _pullButton;
     private Button _addButton;
     private Button _commitButton;
+    private Button _colorButton;
+    private Button _pushButton;
     private Scanner _input;
 
     public Main(){
@@ -19,8 +21,10 @@ public class Main extends Frame {
         _path = _input.nextLine();
 
         _pullButton = new Button(java.awt.Color.RED, "Pull", _path, 500, 200);
-        _addButton = new Button(java.awt.Color.BLUE, "Add", _path, 300,200);
-        _commitButton = new Button(Color.MAGENTA, "Commit", _path, 150, 100);
+        _addButton = new Button(java.awt.Color.BLUE, "Add", _path, 200,200);
+        _commitButton = new Button(Color.MAGENTA, "Commit", _path, 300, 200);
+        _pushButton = new Button(Color.GREEN, "Push", _path, 400, 200);
+
     }
 
     public static void main(String[] args) {
